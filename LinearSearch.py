dde8b019f5f825ef0e0d7d898ffe527c8b638154
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-03-05 19:03:28 -0300

    first commit

---
+++ LinearSearch.py
@@ -0,0 +1,16 @@
+import time, numpy as np
+
+searchSize = 10000
+haystack = np.random.randint(10000, size=searchSize)
+
+def linearSearch(haystack, needle):
+    for i in range(len(haystack)):
+        print(f"Looking at: {i}:{haystack[i]}. Needle is: {needle}")
+        if haystack[i] == needle:
+            print(f"Needle {needle} found at index: {i}")
+            return True
+        time.sleep(.1)
+    print(f"No {needle} in the haystack")
+    return False
+
+linearSearch(haystack, 42)
